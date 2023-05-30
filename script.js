8957d4f289fcf72d9b4cf8911ddcab57a9147f1e
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-05-30 09:06:07 -0400

    Update script.js
    Removed debugging

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function DestroyOthers(Self) {
     var UsedIds=["#About","#HowToUse","#TopGames"]
     let KeptId=UsedIds.splice(Self,1)
-    console.log(UsedIds)
     for (var i=0;i<UsedIds.length;i++) {
-        console.log("Hiding: "+UsedIds[i])
         $(UsedIds[i]).hide()
     }
     $(KeptId[0]).show();
