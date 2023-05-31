5bdb263be089c979d865fb2e9a0470293dd95e6a
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2023-05-31 11:55:43 -0400

    Fixed stylesheet and classes, and preparing to add dropdown

--- script.js
+++
@@ -1,8 +0,0 @@
-function DestroyOthers(Self) {
-    var UsedIds=["#About","#HowToUse","#TopGames"]
-    let KeptId=UsedIds.splice(Self,1)
-    for (var i=0;i<UsedIds.length;i++) {
-        $(UsedIds[i]).hide()
-    }
-    $(KeptId[0]).show();
-}
